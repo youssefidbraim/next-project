ba9b45cbda4912dc07daf9cae65469db53278a98
youssef idbraim <youssef.idbraim@gmail.com>
2021-05-19 01:51:28 +0100

    AAA

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! youssef</a>
+          Welcome to <a href="https://nextjs.org">Next.js! RAJAE</a>
         </h1>
 
         <p className={styles.description}>
@@ -22,7 +22,7 @@ export default function Home() {
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <p>Find in-depth information about Next.js features and API. rajae</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
